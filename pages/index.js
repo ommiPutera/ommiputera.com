754f9576d5dd3c4f97140b0bb063a0dfa6a07228
ommiputera <omiputrakarunia@gmail.com>
2022-04-01 23:33:04 +0700

    Setting tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Ommi Putera</h1>
+      <h1 className="">Ommi Putera</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black": "#202124",
+        "dark-gray": "#BCBCBC",
+        "dark-gray-500": "#C4C4C4",
+        "light-gray": "#FAFAFA",
+        "light-gray-500": "#E8E8E8",
+        "brown": "#BD9354",
+        "white": "#FFFFF",
+        "blue": "#4285F4"
+      }
+    },
   },
   plugins: [],
 }
