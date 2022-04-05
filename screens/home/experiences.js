89c315c80a64e7d5556c91192ab67412ffe56f97
ommiputera <omiputrakarunia@gmail.com>
2022-04-05 07:33:44 +0700

    update: navigation guide and bug fixing

---
+++ screens/home/experiences.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Experiences() {
+  return (
+    <div className="min-h-screen">
+      <h1>Experiences</h1>
+    </div>
+  )
+}
+
+export default Experiences;
---
+++ screens/home/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="min-h-screen">
+      <h1>Footer page</h1>
+    </div>
+  )
+}
+
+export default Footer;
